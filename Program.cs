29be9e6ad672dda4ba81db12b4f2466687496067
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-06-17 11:24:58 +0530

    uc4: using switch case

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ if (isemppr == 1)
     int dailywage = Wageperhour * fulldayhour;
     Console.WriteLine("Daily wage of the employee is = " + dailywage);
     Console.WriteLine("Enter 1 - Part time & 2- for full time employee");
+    int choice = Convert.ToInt16(Console.ReadLine());
+    int day_hr = 0;
+     switch (choice)
+    {
+        case 1:
+            day_hr = 4;
+            break;
+        case 2:
+            day_hr = 8;
+            break;
+        default:
+            day_hr = 0;
+            break;
+    }
 }
 else
 {
