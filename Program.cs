1a34d854e059a29487b4cef77217f4bbb2b7297c
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-06-17 11:07:56 +0530

    uc1: check for emp present or absent

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ int isemppr = random.Next(0, 2);
 if (isemppr == 1)
 {
     Console.WriteLine("Prsent");
+    int Wageperhour = 20;
+    int fulldayhour = 8;
+    int dailywage = Wageperhour * fulldayhour;
+    Console.WriteLine("Daily wage of the employee is = " + dailywage);
 }
 else
 {
